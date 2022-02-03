65daeca3fb16356d04d33754eb8217e25a2fc6d3
hammal <87265940+hammaal@users.noreply.github.com>
2022-02-03 19:14:20 +0500

    Update communication.py

--- communication.py
+++ communication.py
@@ -6,15 +6,18 @@ import numpy as np
 sample_rate=3200
 length = 5
 
+def read_data():
+	os.system(f'sudo adxl345spi -t {length_s} -f {sample_rate} -s out.csv')
+	acc_data = np.genfromtxt('out.csv', delimiter=',', names=True)
+	
+	for i in range(len(acc_data['time'])):
+		ser.write(str(acc_data['time'][i]).encode('ascii'))
+		ser.write(str(acc_data['z'][i]).encode('ascii'))
+	
+	ser.write('f'.encode('ascii'))
+	
 ser = serial.Serial('/dev/ttyS0', 9600)
 while True:
 	recieved_data = ser.read()
 	if recieved_data == 'r':
-		os.system(f'sudo adxl345spi -t {length_s} -f {sample_rate} -s out.csv')
-		acc_data = np.genfromtxt('out.csv', delimiter=',', names=True)
-
-		for i in range(len(acc_data['time'])):
-			ser.write(acc_data['time'][i].encode('ascii'))
-			ser.write(acc_data['z'][i].encode('ascii'))
-
-		ser.write('f'.encode('ascii'))
+		read_data()
